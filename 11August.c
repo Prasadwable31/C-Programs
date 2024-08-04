bfe364ca949af74de277a82278baa50b7e04d278
Prasadwable31 <113745122+Prasadwable31@users.noreply.github.com>
2024-08-04 18:03:57 +0530

    Add files via upload

---
+++ 11August.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main ()
+{
+	int a;
+	printf(" %d\n Hello\n World",a);
+	
+	return 0;
+}
---
+++ Addition.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main()
+{
+	int No1 = 10;   // No1 is a variable to store temperory value
+	
+	int No2 = 11;   // No1,No2 is a varable of type integer where values are inatilized
+	
+	int Ans = 0;   // inatilized a variable by 0 to not enter any other value and also for good programming practice
+//	char Ans = \0;
+//	float Ans = 0.0;
+//	double Ans = 0.0;
+	
+	Ans = No1 + No2;
+	// now the value change by 21.
+	
+	printf("%d",Ans);   // %d is specifier d = decimal 
+	
+	return 0;    // for O.S 
+}
+
+
+
+
+//implicite = I remove you
+//explicite = You remove by your self
