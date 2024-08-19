3046c8acc70ff0644260feebb9b9e6d0a183a414
Prasadwable31 <113745122+Prasadwable31@users.noreply.github.com>
2024-08-19 21:55:35 +0530

    Add files via upload

---
+++ Hello.c
@@ -0,0 +1,19 @@
+#include<stdio.h> 
+
+int main(){
+	
+    printf("Jay Ganesh...\n");
+    
+    return 0;          
+}   
+
+
+
+//derived = created by someone    coffee
+//primitive  = already define    milk        it is also known as buitlin
+//user define = user la phage tasa create karne datatype   photocake
+
+
+
+//reference = toppan nav
+//boolean = yes or no
