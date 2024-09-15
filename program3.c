9264106c11ea811e737624d00a03671147f6bf35
Prasad Wable <prasadwable31@gmail.com>
2024-09-15 12:51:12 +0530

    Add files via upload

---
+++ program3.c
@@ -0,0 +1,22 @@
+// Program to print 5 to 1 numbers on screen
+
+#include<stdio.h>
+
+void Display()
+{
+	int i = 0;
+	
+	printf("Reverse Number is : \n");
+	
+	for(i = 5; i>0; i--)
+	{
+		printf("%d\n",i);
+	}
+}
+
+int main()
+{
+	Display();
+	
+	return 0;
+}
