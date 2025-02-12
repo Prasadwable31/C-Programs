9a375049645f888c83751b60eaad8e681d428e3f
Prasad Wable <prasadwable31@gmail.com>
2025-02-12 21:52:36 +0530

    Add files via upload

---
+++ program68.c
@@ -0,0 +1,39 @@
+// Write a program to display the elements of array which are divisible by 5 and 2.
+// Input:  Arr =  55 30	54	85	10	98	36	73	60
+// Output:   30 10	60
+
+
+#include<stdio.h>
+
+void Divisible(int iNo, int iArr[])
+{
+    int iCnt = 0;
+
+    for (iCnt = 0; iCnt < iNo; iCnt++)
+    {
+        if ((iArr[iCnt] % 2) == 0  && (iArr[iCnt] % 5) == 0)
+        {
+            printf("%d ",iArr[iCnt]);
+        }
+    }
+}
+
+int main()
+{
+    int iCount = 0, i = 0;
+
+    printf("Enter the number of elements : ");
+    scanf("%d",&iCount);
+
+    int Arr[iCount];
+
+    printf("Enter the elements of array : ");
+    for(i = 0; i < iCount; i++)
+    {
+        scanf("%d",&Arr[i]);
+    }
+
+    Divisible(iCount, Arr);
+
+    return 0;
+}
