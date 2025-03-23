f44c4126ebc97c9a7b87b111fa6c0fd9091f0ae6
Prasad Wable <prasadwable31@gmail.com>
2025-03-23 21:51:54 +0530

    Add files via upload

---
+++ program109.c
@@ -0,0 +1,50 @@
+// Write a program to display a following pattern
+// Input = Rows - 3 Colunm - 4
+// Output = * * * *
+//          * @ @ *
+//          * @ @ *
+//          * * * * 
+
+
+#include<stdio.h>
+
+void Pattern(int iRow, int iColumn)
+{
+    int i = 0, j = 0;
+
+    int No = 0;
+
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = 1; j <= iColumn ; j++)
+        {
+
+            if((i > 1 && i < 4) && (j > 1 && j < 4))
+            {
+                printf("@ ");
+            }
+            else
+            {
+                printf("* ");
+            }
+
+        }
+            printf("\n");
+    }
+
+}
+
+int main()
+{
+    int row = 0, column = 0;
+
+    printf("Enter the number of rows : ");
+    scanf("%d",&row);
+
+    printf("Enter the number of colunms : ");
+    scanf("%d",&column);
+
+    Pattern(row, column);
+
+    return 0;
+}
